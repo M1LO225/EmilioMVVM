f1c3808d96bfc71ffb263839df6fe150b1f62533
M1LO225 <emilio.guerrero.argoti@udla.edu.ec>
2024-12-21 17:32:17 -0500

    Agregar archivos de proyecto.

---
+++ EmilioMVVM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EmilioMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
